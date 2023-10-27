fd20be541e589fbbc6b09c6becc5786e8e32e85a
ShutaSone <soneshuta.webdesign@gmail.com>
2023-10-28 08:40:20 +0900

    パス開通

--- app.js
+++ app.js
@@ -3,11 +3,13 @@ const mysql = require('mysql');
 
 const app = express();
 
+app.use(express.static('public'));
+
 const connection = mysql.createConnection({
   host: 'localhost',
   user: 'root',
   password: 'shuta0111',
-  database: 'list_app'
+  database: 'weight_management'
 });
 
 connection.connect((err) => {
@@ -23,9 +25,21 @@ app.get('/', (req, res) => {
     'SELECT * FROM users',
     (error, results) => {
       console.log(results);
-      res.render('hello.ejs');
+      res.render('main.ejs');
     }
   );
 });
+app.get('/target',(req,res) => {
+  res.render('target.ejs');
+});
+app.get('/login',(req,res) => {
+  res.render('login.ejs');
+});
+app.get('/registration',(req,res) => {
+  res.render('registration.ejs');
+});
 
-app.listen(3000);
+const port = 3002; // ポート番号を変数に指定
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
